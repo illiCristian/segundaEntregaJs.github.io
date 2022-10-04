3223f1d666c8ae620c4329e8042242200de79dc7
illiCristian <svrk73@gmail.com>
2022-10-04 17:24:41 -0300

    segundo commit

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const vaciarCarrito = document.getElementById("vaciar-carrito");
 const contadorCarrito = document.getElementById("contador");
 const contadorCarrito1 = document.getElementById("contador1");
 const precioTotal = document.getElementById("precio-total");
+const btnRestar = document.getElementById("btn-restar");
+
 
 let carrito = [];
 
@@ -14,14 +16,6 @@ let carrito = [];
        actualizarCarrito();
    }
   })
-
-//  let carritoStorage = JSON.parse(localStorage.getItem("carrito"));
-
-//   if(carritoStorage){
-//     carrito = carritoStorage;
-//   }else{
-//     console.log('no esta guardado');
-//   }
 
 class Producto{
     constructor(id,nombre,descripcion,precio,img,cantidad){
@@ -36,22 +30,22 @@ class Producto{
 
 const productos = [];
 
-let producto1 = new Producto(1,'Bolsitas Cumpleaños','papel fotografico 120 gr, 17 cm x 10,5 cm x 3,5 cm',150,"https://madaradesign.netlify.app/img/disenos%20personalizados/linea1%20(3).jpeg",0);
+let producto1 = new Producto(1,'Bolsitas Cumpleaños','Papel fotografico 120 gr, 17 cm x 10,5 cm x 3,5 cm',150,"https://madaradesign.netlify.app/img/disenos%20personalizados/linea1%20(3).jpeg",0);
 let producto2 = new Producto(2,'Pokemones de apego','20 cm de alto, relleno de vellon siliconado',350,"https://i.ibb.co/v30CSMN/Whats-App-Image-2022-09-28-at-10-05-41-1.jpg",0);
-let producto3 = new Producto(3,"Vaso Acrilico","personalizado con vinilo holografico",500,"https://i.ibb.co/mvTRtfF/vaso.jpg",0);
+let producto3 = new Producto(3,"Vaso Acrilico","Personalizado con vinilo holografico",500,"https://i.ibb.co/mvTRtfF/vaso.jpg",0);
 let producto4 = new Producto(4,'Muñecos de apego','20 cm aprox, rellenos de vellon siliconado',350,"https://i.ibb.co/Lx7v4y5/Whats-App-Image-2022-09-28-at-10-05-42.jpg",0);
 let producto5 = new Producto(5,'Cartuchera','21cm x 12cm personalizable',400,"https://i.ibb.co/fqQsVxw/Whats-App-Image-2022-09-28-at-10-05-41.jpg",0);
-let producto6 = new Producto(6,'Taza HellFire Club!','ceramica',1000,"https://i.ibb.co/XFQyVqQ/Whats-App-Image-2022-09-28-at-10-05-42-2.jpg",0);
-let producto7 = new Producto(7,'Gorra trucker','parche bordado personalizable',1000,"https://i.ibb.co/QHmBJp3/Whats-App-Image-2022-09-28-at-10-05-42-1.jpg",0);
+let producto6 = new Producto(6,'Taza HellFire Club!','Ceramica',1000,"https://i.ibb.co/XFQyVqQ/Whats-App-Image-2022-09-28-at-10-05-42-2.jpg",0);
+let producto7 = new Producto(7,'Gorra trucker','Parche bordado personalizable',1000,"https://i.ibb.co/QHmBJp3/Whats-App-Image-2022-09-28-at-10-05-42-1.jpg",0);
 let producto8 = new Producto(8,'Leo Messi + Copa Mundial','20 cm relleno de vellon siliconado',600,"https://i.ibb.co/sQYxdgd/Whats-App-Image-2022-09-28-at-10-05-40.jpg",0);
+let producto9 = new Producto(9,'Porta Sube','Personalizados con vinilo',250,"https://i.ibb.co/cFmMk41/Whats-App-Image-2022-09-29-at-18-39-54.jpg",0);
 
 
-productos.push(producto1,producto2,producto3,producto4,producto5,producto6,producto7,producto8);
+productos.push(producto1,producto2,producto3,producto4,producto5,producto6,producto7,producto8,producto9);
 
 console.log(productos);
 
 let div = document.getElementById('contenedor-div');
-
 
 productos.forEach(el=>{
     let productoRenderizado = document.createElement('div');
@@ -63,7 +57,7 @@ productos.forEach(el=>{
                 <h5 class="card-title text-decoration-underline text-center">${el.nombre}</h5>
                 <p class="card-text text-center">${el.descripcion}.</p>
                 <p class="card-text text-center ">Precio: $${el.precio}.</p>
-                <a href="#" class="btn btn-primary btn-carrito" id="${el.id}">Agregar al carrito</a>
+                <a href="#" class="btn btn-primary btn-primary btn-carrito" id="${el.id}">Agregar al carrito</a>
             </div>
         </div>
     </div>
@@ -90,33 +84,43 @@ productos.forEach(el=>{
             
         }
         actualizarCarrito();    
-     console.log(carrito);       
+     console.log(carrito);  
+     
+    //   const btnSumar = document.getElementsByClassName("boton-sum");
+    //   btnSumar.addEventListener("click", () => {
+    //       console.log((el.id));
+    //   });
     });
-
+   
     
 })
 
-
 const actualizarCarrito = () => {
-
     contenedorCarrito.innerHTML="";
     carrito.forEach(prod =>{
     
     const carritoActualizado = document.createElement("div");
      carritoActualizado.innerHTML =`
-     <div class="card mb-3" style="max-width: 600px;">
+     <div class="card mb-3 text-bg-dark" style="max-width: 500px;">
          <div class="row g-0">
              <div class="col-md-4 align-items-center">
                 <img src="${prod.img}" class="img-fluid rounded-start" alt="...">
     </div>
         <div class="col-md-8">
         <div class="card-body card-carrito">
-            <h5 class="card-title">${prod.nombre}</h5>
-            <p class="card-text">${prod.descripcion}.</p>
-            <p class="card-text"><small class="text-muted">Cantidad: ${prod.cantidad}</small></p>
-            <p class="card-text"><small class="text-muted">Precio: $${prod.precio}</small></p>
-            
+            <h5 class="card-title text-decoration-underline">${prod.nombre}</h5>
+            <p class="card-text">${prod.descripcion}.<br>
+            Cantidad: ${prod.cantidad}</p>
+            Precio: $${prod.precio}</p>
+            <div class="div-carrito1">
             <button id="trash" onclick="borrarItemCarr(${prod.id})"><i class="fas fa-trash-alt mr-3"></i></button>
+            <button class="btn btn-danger btn-sm boton-sumar" onclick="sumarItemCarr(${prod.id})" id="${prod.id}">
+                +
+            </button>
+            <button class="btn btn-danger btn-sm" onclick="restarItemCarr(${prod.id})" id="btn-restar">
+                -
+            </button>
+            </div>
         </div>
         </div>
     </div>
@@ -136,13 +140,35 @@ const actualizarCarrito = () => {
 }
 
 const borrarItemCarr = (prod) => {
-    const item = carrito.find(el=> el.id === prod.id);
+    const item = carrito.find(el=> el.id === prod);
+    console.log(item);
     const indice = carrito.indexOf(item);
     carrito.splice(indice,1);
     actualizarCarrito();
+}
+
+const sumarItemCarr = (prod) =>{
+    const item = carrito.find(el => el.id === prod);
+    precio = productos.find(el => el.id === prod)
+    item.cantidad = item.cantidad + 1;
+    item.precio = item.precio + precio.precio;
+    actualizarCarrito();   
+}
+
+const restarItemCarr = (prod) =>{
+    const item = carrito.find(el => el.id === prod);
+    precio = productos.find(el => el.id === prod);
+    if (item.cantidad <= 1){
+        borrarItemCarr(prod);
+        actualizarCarrito();
+    }else{
+    item.cantidad = item.cantidad - 1;
+    item.precio = item.precio - precio.precio;
+    actualizarCarrito();}
 }
 
 vaciarCarrito.addEventListener("click", ()=> {
     carrito.length = 0;
     actualizarCarrito();
 })
+
